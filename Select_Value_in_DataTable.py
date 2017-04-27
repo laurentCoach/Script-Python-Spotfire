723d705584119b066734185b3b0fcf1d27c26b79
laurentCoach <laurentcesaro17@gmail.com>
2017-04-27 15:23:22 +0200

    Display_DataTable_Value_In_DocumentProperties.py
    Display value table in Document.Properties

--- Select_Value_in_DataTable.py
+++ Select_Value_in_DataTable.py
@@ -1,12 +1,15 @@
 #Laurent Cesaro
-#Date 16/11/2016
+
 #This script permit to recover a row in a datatable and display it in a label (Document.Properties)
 
-
+#Inport library
 from Spotfire.Dxp.Data import DataValueCursor
 
+#Create cursor on table
 myStatusCursor = DataValueCursor.CreateFormatted(Document.Data.Tables["Table_Name"].Columns["Column_Name"])
+#Select marked row
 markedRows = Document.Data.Markings["Marking"].GetSelection(Document.Data.Tables["Table_Name"]).AsIndexSet()
 
+#Add in your Document.Properties the table value
 for row in Document.Data.Tables["Table_Name"].GetRows(markedRows, myStatusCursor):
 	Document.Properties["Porprety_Name"] = myStatusCursor.CurrentValue
