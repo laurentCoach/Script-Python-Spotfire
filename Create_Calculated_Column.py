15ecd9d5bbcfb40eda3f6fb94dd8b28affbdbee2
laurentCoach <laurentcesaro17@gmail.com>
2017-04-27 15:16:08 +0200

    Create a calculated column in a table

--- Create_Calculated_Column.py
+++ Create_Calculated_Column.py
@@ -2,6 +2,11 @@
 #Date 17/03/2017
 #Create a calculated column
 
+#Import librarie
 from Spotfire.Dxp.Data import CalculatedColumn
+
+#Select your table
 cols = Document.Data.Tables["MyTable"].Columns
+
+#Define column name and expression
 cols.AddCalculatedColumn("ThisIsACalculatedColumn", "Expression")
