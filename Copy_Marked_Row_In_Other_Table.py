fdf1c7974789838f2863f6bab4fee5268aef54a2
laurentCoach <laurentcesaro17@gmail.com>
2017-04-27 15:11:42 +0200

    Update and rename CopyRowInOtherTable.py to Copy_Marked_Row_In_Other_Table.py

--- Copy_Marked_Row_In_Other_Table.py
+++ Copy_Marked_Row_In_Other_Table.py
@@ -1,16 +1,17 @@
+#Laurent Cesaro
+
+#Import Libraries
 from Spotfire.Dxp.Data import AddRowsSettings
 from Spotfire.Dxp.Data.Import import DataTableDataSource
 
 #Source Data Table
-
-sourceDataTable= Document.Data.Tables["NEW_CONTROL_LIMIT"]
+sourceDataTable= Document.Data.Tables["Table1"]
 
 #Mark rows on the source
-
 dataSelection=Document.ActiveMarkingSelectionReference
-dataSource=DataTableDataSource(Document.Data.Tables["NEW_CONTROL_LIMIT"],dataSelection)
+dataSource=DataTableDataSource(Document.Data.Tables["Table1"],dataSelection)
 
 #Copy rows to new data table
-destinationDataTable=Document.Data.Tables["USER_CONTROL_LIMIT"]
+destinationDataTable=Document.Data.Tables["Table2"]
 rowsettings=AddRowsSettings(destinationDataTable,dataSource)
 destinationDataTable.AddRows(dataSource,rowsettings)
